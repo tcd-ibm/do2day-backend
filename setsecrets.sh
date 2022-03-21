ad4299c1ee391d70dbadc8edac3b4e8012ea31b9
briantwhelan <whelanb8@tcd.ie>
2022-03-21 11:21:26 +0000

    Add scripts to initialise secrets
    This commit uses scripts taken from a RedHat project to initialise the
    required secrets fro configuring OpenShift for GitHub Actions.

---
+++ setsecrets.sh
@@ -0,0 +1,12 @@
+TOKEN=$(oc whoami --show-token)
+SERVER=$(oc whoami --show-server )
+
+gh secret set OPENSHIFT_SERVER -b $SERVER
+gh secret set OPENSHIFT_TOKEN -b $TOKEN
+if [ -z $MY_QUAY_PW ]
+then
+echo  $0: No MY_QUAY_PW set, please set MY_QUAY_PW to your registry password.
+exit
+else 
+gh secret set REGISTRY_PASSWORD -b $MY_QUAY_PW
+fi
